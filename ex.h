72433808895926378a531dfa7c60d71c4f6730d0
Martin Sova <martinkevinsova@gmail.com>
2018-01-02 20:47:42 +0100

    Removing use of qtservice

---
+++ ex.h
@@ -0,0 +1,29 @@
+#ifndef EX_H
+#define EX_H
+
+#include <QAbstractItemModel>
+
+class ex : public QAbstractItemModel
+{
+    Q_OBJECT
+
+public:
+    explicit ex(QObject *parent = 0);
+
+    // Header:
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
+
+    // Basic functionality:
+    QModelIndex index(int row, int column,
+                      const QModelIndex &parent = QModelIndex()) const override;
+    QModelIndex parent(const QModelIndex &index) const override;
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+private:
+};
+
+#endif // EX_H
